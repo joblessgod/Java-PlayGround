3be4c90bbed7086b8dea43e0651dc657da4abf55
JobLessGod <iamjoblessgod@gmail.com>
2025-09-10 13:48:55 +0545

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] arg){
+        System.out.print("Hello, Java 1");
+        System.out.print("Hello, Java 2");
+        System.out.print("Hello, Java 3");
+        System.out.print("Hello, Java 4");
+        System.out.print("Hello, Java 5");
+    }
+}
