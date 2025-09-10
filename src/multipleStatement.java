2d3729b662741dcb945560dbbfaf1fb9378461a2
JobLessGod <iamjoblessgod@gmail.com>
2025-09-10 15:36:24 +0545

    multipleStatement

---
+++ src/multipleStatement.java
@@ -0,0 +1,15 @@
+/*
+* More Java statements
+* This code computers square root
+*/
+
+import java.lang.Math;
+class SquareRoot{
+    public static void main (String[] args){
+        double x =256;
+        double y;
+        y = Math.sqrt(x);
+        System.out.print("y = " + y);
+    }
+
+}
