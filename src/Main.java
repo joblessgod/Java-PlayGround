d58f4f7f0e66daa7ab239b2106fcf8ab358dce61
JobLessGod <iamjoblessgod@gmail.com>
2025-09-10 13:52:37 +0545

    added more java numbers.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         System.out.print("Hello, Java 3");
         System.out.print("Hello, Java 4");
         System.out.print("Hello, Java 5");
+        System.out.print("Hello, Java 6");
+        System.out.print("Hello, Java 7");
+        System.out.print("Hello, Java 8");
+        System.out.print("Hello, Java 9");
+        System.out.print("Hello, Java 10");
     }
 }
