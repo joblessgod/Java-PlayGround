537e91a087fb387e33a44748c251ea64a0bfe925
JobLessGod <iamjoblessgod@gmail.com>
2025-09-10 16:35:05 +0545

    In Displaying.java made user input

--- src/Displaying.java
+++ src/Displaying.java
@@ -1,13 +1,22 @@
+import java.io.DataInputStream;
+
 class Displaying {
     public static void main(String[] args){
-        System.out.println("Screen Displaying");
-        for (int i = 1; i <= 9; i++){
+        System.out.print("Enter a number: ");
+        DataInputStream in = new DataInputStream(System.in);
+        int num = 0;
+        try {
+            num = Integer.parseInt(in.readLine());
+        } catch (Exception e){}
+        System.out.print("You entered: " + num);
+        System.out.println("\n=== Screen Displaying ===");
+        for (int i = 1; i <= num; i++){
             for (int j = 1; j <= i; j++){
-                System.out.print(i);
+                System.out.print(i + " ");
             }
-            System.out.println("\n- - -");
+            System.out.println(" ");
         }
-        System.out.println("Screen Display Done");
+        System.out.println("=== Screen Display Done ===");
     }
 
 
