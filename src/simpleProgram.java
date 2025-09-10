5a504590eb932c82a4fbf88f2c3ffa37824d27f4
JobLessGod <iamjoblessgod@gmail.com>
2025-09-10 15:55:33 +0545

    Another simple program for testing

---
+++ src/simpleProgram.java
@@ -0,0 +1,7 @@
+public class simpleProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Welcome to the world if java.");
+        System.out.println("Let us learn Java.");
+    }
+}
